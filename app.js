afcee261b260a4cedcba3c35a0d8ef279f52a67c
aidenakash <samakash413@gmail.com>
2024-01-10 14:16:47 +0530

    commit

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const app = express();
 const dotEnv = require("dotenv");
 const mongoose = require("mongoose");
 
+app.use(express.json());//Parsing Incoming JSON Data:
+app.use((req,res,next)=>{ //Middleware for Request Logging and Timing:
+  req.requestTime = new Date().toISOString();
+  console.log(req.headers);
+  next();
+
+})
+
 dotEnv.config({ path: "./config.env" });
 
 mongoose
@@ -11,7 +19,13 @@ mongoose
     console.log("DB_CONNECTED!!!");
   })
   .catch((err) => {
-    console.error("MongoDB connection error:", err);
+    // console.error("MongoDB connection error:", err.message);
+    if (
+      err.message ==
+      "querySrv ECONNREFUSED _mongodb._tcp.cluster0.b9jwp.mongodb.net"
+    )
+      console.log("CHECK YOUR NETWORK CONNECTION");
+    else console.log("MongoDB Error".err.message);
   });
 
 const port = process.env.PORT;
